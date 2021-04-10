8c038a87b7bf1dea67e4c5fece015ade916e0104
Shojin Masuda <shomasd@gmail.com>
2021-04-10 17:37:22 +0900

    simple calc implemented

---
+++ app/Main.hs
@@ -0,0 +1,81 @@
+{-# LANGUAGE RankNTypes #-}
+
+module Main where
+
+import Control.Monad
+import qualified Data.List as L
+import Data.Map (Map)
+import qualified Data.Map as M
+import Data.Text (Text)
+import qualified Data.Text as T
+
+data Value
+  = Num Int
+  | Str Text
+  deriving (Show, Eq)
+
+data TypeTag = Number | Strng deriving (Show, Eq)
+
+type Sym = Text
+
+type Env = Map Sym Expr
+
+data Expr
+  = Constant Value
+  | Atom Text
+  | List [Expr]
+  deriving (Show, Eq)
+
+type Error = Text
+
+showText :: (Show a) => a -> Text
+showText = T.pack . show
+
+evalToNum :: Env -> Expr -> Either Error Int
+evalToNum env x = case eval env x of
+  Right (Constant (Num v)) -> pure v
+  _ -> Left $ "type error, num expected on " <> showText x
+
+evalInfix :: forall a. Env -> (Expr -> a -> Either Error Expr) -> [Expr] -> (Env -> Expr -> Either Error a) -> Either Error Expr
+evalInfix env op args innerEval = do
+  if null args
+    then Left "no arg"
+    else do
+      let (h : t) = args
+      foldM
+        ( \acc arg -> do
+            arg' <- innerEval env arg
+            op acc arg'
+        )
+        h
+        t
+
+evalMath :: Env -> (Int -> Int -> Int) -> [Expr] -> Either Error Expr
+evalMath env op args = do
+  let op' :: Expr -> Int -> Either Error Expr
+      op' l r = do
+        l' <- evalToNum env l
+        pure . Constant . Num $ op l' r
+  evalInfix env op' args evalToNum
+
+pattern OpList x xs = (List ((Atom x) : xs))
+
+pattern QuoteList xs = OpList "quote" xs
+
+pattern N x = Constant (Num x)
+
+eval :: Env -> Expr -> Either Error Expr
+eval env v@(Constant _) = pure v
+eval _ (QuoteList xs) = pure $ List xs
+eval env (OpList x xs) = evalOpExpr env x xs
+
+evalOpExpr :: Env -> Sym -> [Expr] -> Either Error Expr
+evalOpExpr env "+" args = evalMath env (+) args
+evalOpExpr env "-" args = evalMath env (-) args
+evalOpExpr env "*" args = evalMath env (*) args
+evalOpExpr env "/" args = evalMath env div args -- TODO devide by zero error
+evalOpExpr env op args = Left $ "not implemented: " <> showText op <> " " <> showText args
+
+main :: IO ()
+main = do
+  print $ eval M.empty $ OpList "+" [N 3, N 1, OpList "*" [N 19, N 2]]
