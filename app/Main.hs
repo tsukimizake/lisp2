59ae42d38b02994d7ad91cb729468435ce867083
Shojin Masuda <shomasd@gmail.com>
2021-04-11 23:40:37 +0900

    strict evaluaton

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ import qualified Data.List as L
 import qualified Data.Map as M
 import Data.Text (Text)
 import qualified Data.Text as T
+import Debug.Trace
 import Parser
 import Types
 
@@ -38,16 +39,26 @@ evalMath env op args = do
         pure . Constant . Num $ op l' r
   evalInfix env op' args evalToNum
 
-pattern OpList x xs = (List ((Atom x) : xs))
+pattern OpList x xs = (List (x : xs))
 
-pattern QuoteList xs = OpList "quote" xs
+pattern QuoteList xs = OpList (Atom "quote") xs
 
 pattern N x = Constant (Num x)
 
 eval :: Env -> Expr -> Either Error Expr
 eval env v@(Constant _) = pure v
+eval _ x@(Atom _) = pure x
 eval _ (QuoteList xs) = pure $ List xs
-eval env (OpList x xs) = evalOpExpr env x xs
+eval env (List []) = do
+  pure nil
+eval env (List [x]) = do
+  eval env x
+eval env (OpList x xs) = do
+  xs' <- mapM (eval env) xs
+  x' <- eval env x
+  case x' of
+    Atom a -> evalOpExpr env a xs'
+    _ -> Left "TODO: non-atom function"
 
 evalOpExpr :: Env -> Sym -> [Expr] -> Either Error Expr
 evalOpExpr env "+" args = evalMath env (+) args
@@ -56,6 +67,9 @@ evalOpExpr env "*" args = evalMath env (*) args
 evalOpExpr env "/" args = evalMath env div args -- TODO devide by zero error
 evalOpExpr env op args = Left $ "not implemented: " <> showText op <> " " <> showText args
 
+nil :: Expr
+nil = List []
+
 main :: IO ()
 main = do
-  print $ eval M.empty $ OpList "+" [N 3, N 1, OpList "*" [N 19, N 2]]
+  print $ eval M.empty $ OpList (Atom "+") [N 3, N 1, OpList (Atom "*") [N 19, N 2]]
