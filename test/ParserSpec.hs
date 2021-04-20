4cafdb5a54a70bdfd73129d5a02cb90334605a9f
Shojin Masuda <shomasd@gmail.com>
2021-04-21 07:20:56 +0900

    add test

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -14,5 +14,6 @@ spec :: Spec
 spec = describe "" do
   it "parse negative int" do
     testParser "-1" (N (-1))
+    testParser "(-1)" (List [N (-1)])
     testParser "(- 1 2)" (List [Atom "-", N 1, N 2])
     testParser "(- a b )" (List [Atom "-", Atom "a", Atom "b"])
