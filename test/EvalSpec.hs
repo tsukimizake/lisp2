8d8700b493b081deff74615eaaf6bf551c91494b
Shojin Masuda <shomasd@gmail.com>
2021-05-07 19:25:10 +0900

    add evalspec

--- test/EvalSpec.hs
+++ test/EvalSpec.hs
@@ -1,10 +1,22 @@
 module EvalSpec where
 
+import Control.Monad.IO.Class
+import Data.Text
 import Eval
 import Parser
 import Test.Hspec
+import Types
+import GHC.IO (unsafePerformIO)
+
+testEval :: Env -> Text -> Expr -> Expectation
+testEval env input expect = do
+  case parseExpr input of
+    Right expr -> do
+      evaled <- runIOThrows $ eval env expr
+      evaled `shouldBe` Right expect
+    Left err -> error $ show err
 
 spec :: Spec
 spec = describe "eval" do
   it "can eval" do
-    1 `shouldBe` 1
+    testEval (unsafePerformIO nullEnv) "(+ 1 2 -2 (- 0 1))" (N 0)
